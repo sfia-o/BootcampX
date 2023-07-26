41ee31ba6a07e0524b2f02a7ba0da297abb03b21
sfia-o <sofia.vl.oliveira@gmail.com>
2023-07-26 01:39:19 +0000

    Initial commit assignments

--- migrations/students_cohorts.sql
+++ migrations/students_cohorts.sql
@@ -9,7 +9,7 @@ CREATE TABLE students (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255),
-  phone VARCHAR(30),
+  phone VARCHAR(32),
   github VARCHAR(255),
   start_date DATE,
   end_date DATE,
