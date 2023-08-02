f6aa13430c21f35a46af414f209dfe29c5cbe722
sfia-o <sofia.vl.oliveira@gmail.com>
2023-08-02 19:05:57 +0000

    Add command line arguments to query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,17 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const args = process.argv.slice(2);
+const cohort = args[0];
+const maxResults = args[1];
+
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-LIMIT 5;
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [`%${cohort}%`, maxResults])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
